e8310e3c7f2d1bc34f8f65943329df95fc03f4bf
JXRXMXX <76504774+JXRXMXX@users.noreply.github.com>
2021-02-17 15:38:30 +0700

    Add files via upload

---
+++ Appheader.js
@@ -0,0 +1,11 @@
+function Appheader() {
+    
+return (
+   
+      <header className="App-header">
+          <img className ="header-logo" src ="/pic/got7WEB-95.jpeg"/>
+      
+      </header>
+      );
+      }
+export default Appheader;
---
+++ items.js
@@ -0,0 +1,39 @@
+const items =[
+    {
+      title :"JB",
+      thumbnaiUrl :"/pic/got72.jpg"
+    },
+    {
+      title :"mark",
+      thumbnaiUrl :"/pic/got74.jpeg"
+      
+    
+    },
+    {
+      title :"jackson",
+      thumbnaiUrl :"/pic/got77.jpg"
+      
+    
+    },
+    {
+      title :"jinyoung",
+      thumbnaiUrl :"/pic/got73.jpg"
+    
+    },
+    {
+      title :"youngjae",
+      thumbnaiUrl :"/pic/got75.jpg"
+    
+    },
+    {
+      title :"yugyeom",
+      thumbnaiUrl :"/pic/got76.jpg"
+    
+    },
+    {
+      title :"bambam",
+      thumbnaiUrl :"/pic/got71.jpg"
+    
+    }
+  ];
+  export default items;
